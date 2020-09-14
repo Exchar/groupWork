f794a3930835f7078eb202acb489e315d8f50466
unknown <zyx17726564730@163.com>
2020-09-14 08:51:05 +0800

    9.14

---
+++ 郑愉心/郑愉心-9.14/Manager/vueproject/src/views/Home.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="home">
+    <el-container>
+  <el-header>
+    <el-row>
+      <el-col :span="6">
+        <h3>幼儿园管理系统</h3>
+      </el-col>
+      <el-col :span="12"><p></p></el-col>
+      <el-col :span="6"><p>
+        adam
+      </p>
+      </el-col>
+    </el-row>
+  </el-header>
+  <el-container>
+    <el-aside width="200px"><left-menu /></el-aside>
+    <el-main>
+      <router-view></router-view>
+    </el-main>
+  </el-container>
+  <el-footer><p>
+    2020幼儿园管理系统
+  </p></el-footer>
+</el-container>
+  </div>
+</template>
+
+<script>
+import leftMenu from './../components/leftMenu.vue'
+export default {
+  name: 'Home',
+  components: {
+    leftMenu
+  }
+}
+</script>
+<style scoped>
+  .el-header, .el-footer {
+    background-color: #0F4E74;
+    color: white;
+    height: 60px;
+  }
+  .el-footer {
+    text-align: center;
+    line-height: 60px;
+  }
+  .el-header .el-col{
+    height: 60px;
+    line-height: 60px;
+  }
+  .el-header .el-col p{
+    text-align: right;
+  }
+  .el-aside {
+    background-color: #1570A6;
+    color: #333;
+    text-align: center;
+    line-height: 200px;
+  }
+  .el-main {
+    min-height: 500px;
+    background-color: #E9EEF3;
+    color: #333;
+    text-align: center;
+  }
+  body > .el-container {
+    margin-bottom: 40px;
+  }
+</style>
---
+++ 郑愉心/郑愉心-9.14/Manager/vueproject/src/views/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+      <h2>欢迎来到幼儿园管理平台</h2>
+      <p>你可以进行相关的操作</p>
+      <p>图表</p>
+      <p>职工一共100人</p>
+      <p>学生1000人</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'index'
+}
+</script>
+<style scoped>
+</style>
